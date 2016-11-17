e0ce8147ab6c363d981df66f94f7d116f06fb5a9
idillia <mila.antonova@gmail.com>
2016-11-17 10:51:47 -0800

    add textMeWidget, buttons, display images

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
-import HomePage from './components/home/HomePage';
+// import HomePage from './components/home/HomePage';
 import ProfilePage from './components/profile/ProfilePage';
 import DisplayProfilePage from './components/profile/DisplayProfilePage'; //eslint-disable-line import/no-named-as-default
 
