8a4a1a359455e7962c6f7fafeb8368674fd321d5
idillia <mila.antonova@gmail.com>
2016-11-16 18:32:05 -0800

    rename images, create placeholder for images

--- src/components/profile/ProfileListRow.js
+++ src/components/profile/ProfileListRow.js
@@ -1,21 +1,64 @@
 import React, {PropTypes} from 'react';
 import {Link} from 'react-router';
+import _ from 'underscore';
+
 
 const ProfileListRow = ({profile}) => {
   let strImages = profile.personal_archetype[0]
   console.log("prof", profile.image_url);
 
-  // img1 = require("./../../images/new_personas/186/Advocate-m.png");
-  const archImageStyle = {
-    'Advocate': {'backgroundImage':'url(' +   require("./../../images/new_personas/186/Advocate-m.png") +')','height': '80px' },
-    'Go-Getter': {'backgroundImage':'url(' +   require("./../../images/new_personas/186/Go-Getter-m.png") +')','height': '80px'},
-    'Maverick': {'backgroundImage':'url(' +   require("./../../images/new_personas/186/Maverick-m.png") +')', 'height': '80px'} 
+  let pickGender = function(arr) {
+    let genderArray = [];
+    for(var i =0; i<3; i++) {
+      genderArray.push(_.sample(arr, 1)[0]);
+    }
+    return genderArray;
   }
 
+  let theGender = pickGender(["_m", "_f"]);
+
+  console.log(theGender);
+
+
+    let a = "the_advocate";
+    let b = "the_go_getter";
+    let c = "the_maverick"; 
+    const archImageStyle = {
+      'the_advocate': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' },
+      'the_go_getter': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + b + theGender[1] + ".png") +')','height': '80px'},
+      'the_maverick': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + c + theGender[2] +  ".png") +')', 'height': '80px'}, 
+      'the_dreamer': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' },
+      'the_caretaker': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[1] + ".png") +')','height': '80px' },
+      'the_straight_shoooter': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[2] + ".png") +')','height': '80px' },
+      'the_socialite': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' },
+      'the_humanitarian': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[1] + ".png") +')','height': '80px' },
+      'the_idealist': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[2] + ".png") +')','height': '80px' },
+      'the_inventor': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' },
+      'the_mastermind': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[1] + ".png") +')','height': '80px' },
+      'the_protector': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[2] + ".png") +')','height': '80px' },
+      'the_rock': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' },
+      'the_strategist':{'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[1] + ".png") +')','height': '80px' },
+      'the_technician': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[2] + ".png") +')','height': '80px' },
+      'the_visionary': {'backgroundImage':'url(' + require("./../../images/new_personas/186/" + a + theGender[0] + ".png") +')','height': '80px' }
+    }
+
   const archNameStyle = {
-    'Advocate': {'backgroundColor': '#bb51e2'},
-    'Go-Getter': {'backgroundColor': '#81c850'},
-    'Maverick': {'backgroundColor': '#14dfce'}
+    'the_advocate': {'backgroundColor': '#bb51e2'},
+    'the_go_getter': {'backgroundColor': '#81c850'},
+    'the_maverick': {'backgroundColor': '#14dfce'},
+    'the_dreamer': {'backgroundColor': '#14dfce'},
+    'the_caretaker':{'backgroundColor': '#14dfce'},
+    'the_straight_shoooter': {'backgroundColor': '#14dfce'},
+    'the_socialite': {'backgroundColor': '#14dfce'},
+    'the_humanitarian': {'backgroundColor': '#14dfce'},
+    'the_idealist': {'backgroundColor': '#14dfce'},
+    'the_inventor': {'backgroundColor': '#14dfce'},
+    'the_mastermind': {'backgroundColor': '#14dfce'},
+    'the_protector': {'backgroundColor': '#14dfce'},
+    'the_rock': {'backgroundColor': '#14dfce'},
+    'the_strategist': {'backgroundColor': '#14dfce'},
+    'the_technician': {'backgroundColor': '#14dfce'},
+    'the_visionary': {'backgroundColor': '#14dfce'}
   }
 
   return (
@@ -36,17 +79,17 @@ const ProfileListRow = ({profile}) => {
           <div className="archetype-container">
             <div className="row three-arch">
               <div className="col-lg-4 archCol">
-                <div style={archImageStyle.Advocate} className="archetype-image"></div>
-                <div className="archetype-name" style={archNameStyle.Advocate}>ADVOCATE</div>
+                <div style={archImageStyle["the_advocate"]} className="archetype-image"></div>
+                <div className="archetype-name" style={archNameStyle["the_advocate"]}>ADVOCATE</div>
               </div>
               <div className="col-lg-4 archCol">
-                <div className="archetype-image" style={archImageStyle["Go-Getter"]}></div>
-                <div className="archetype-name" style={archNameStyle["Go-Getter"]}>GO GETTER</div>
+                <div className="archetype-image" style={archImageStyle["the_go_getter"]}></div>
+                <div className="archetype-name" style={archNameStyle["the_go_getter"]}>GO GETTER</div>
               </div>
               <div className="col-lg-4 archCol">
-                <div className="archetype-image" style={archImageStyle["Maverick"]}>
+                <div className="archetype-image" style={archImageStyle["the_maverick"]}>
                 </div>
-                <div className="archetype-name" style={archNameStyle.Maverick}>MAVERICK</div>
+                <div className="archetype-name" style={archNameStyle["the_maverick"]}>MAVERICK</div>
               </div>
             </div>
             <span className="strength">Empathic</span>
