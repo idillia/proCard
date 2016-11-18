0c98dcfd2996815023105434c396e0cc45d12d83
idillia <mila.antonova@gmail.com>
2016-11-18 09:03:06 -0800

    move profile image to center

--- src/index.js
+++ src/index.js
@@ -8,8 +8,21 @@ import routes from './routes';
 import {loadProfiles} from './actions/profileActions';
 import configureStore from './store/configureStore';
 import './styles/styles.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+
+
+require("font-awesome-webpack");
+window.$ = window.jQuery = require('jquery');
+const Bootstrap = require('bootstrap-sass');
+Bootstrap.$ = $;
+require('../node_modules/bootstrap-sass/assets/javascripts/bootstrap');
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
+
+
+
+
 
 
 const store = configureStore(); // TODO: if I create server side rendering, I might want to pass initialStage to configureStore();
