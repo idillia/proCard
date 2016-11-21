5bd6b1fa39083f38a17558d0d13f4f189664ab74
idillia <mila.antonova@gmail.com>
2016-11-21 10:16:14 -0800

    add firebase, remove reqiuired from childern PropType

--- src/actions/profileActions.js
+++ src/actions/profileActions.js
@@ -1,6 +1,9 @@
+import Firebase from 'firebase';
 import * as types from './actionTypes';
 import profileApi from '../api/mockProfileApi';
 import {beginAjaxCall, ajaxCallError} from './ajaxStatusActions';
+
+const firebaseProfiles = new Firebase("https://twittercali-ec41d.firebaseio.com/")
 
 //Function below is action creater func and types.LOAD_PROFILES_SUCCESS is an action
 export function loadProfilesSuccess(profiles) {
--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  children: PropTypes.object.isRequired,
+  children: PropTypes.object
 };
 
 function mapStateToProps(state, ownPros) {
