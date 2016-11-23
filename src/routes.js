26a98d62241691c2934db12788c88acf2be61bc6
idillia <mila.antonova@gmail.com>
2016-11-23 14:32:00 -0800

    remove profielist, fix white color on button

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import {Route, IndexRoute} from 'react-router';
 import App from './components/App';
-// import HomePage from './components/home/HomePage';
-import ProfilePage from './components/profile/ProfilePage';
 import DisplayProfilePage from './components/profile/DisplayProfilePage'; //eslint-disable-line import/no-named-as-default
 
 export default (
   <Route path="/" component={App}>
-    <Route path="profiles" component={ProfilePage} />
     <Route path="profile/:id" component={DisplayProfilePage} />
   </Route>  
 );
