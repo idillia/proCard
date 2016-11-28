addc5c4836ffa53541480bc5c3ba50323c1e27f9
idillia <mila.antonova@gmail.com>
2016-11-27 21:13:36 -0800

    add ga to indext.html script

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -12,6 +12,8 @@ fs.readFile('src/index.html', 'utf8', (err, markup) => {
 
   $('head').prepend('<link rel="stylesheet" href="/styles.css">');
   $('head').prepend('<link rel="stylesheet" href="http://d22hhoe037sl7u.cloudfront.net/1.5/css/link_texting.min.css">');
+  $('head').prepend("<script type='text/javascript'>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-39725983-11', 'auto');ga('send', 'pageview'); </script>");
+
 
   fs.writeFile('dist/index.html', $.html(), 'utf8', function(err) {
     if(err){
