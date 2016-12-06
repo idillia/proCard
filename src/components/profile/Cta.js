0bb2445e94014c916f4fdac88af37b309591c86d
idillia <mila.antonova@gmail.com>
2016-12-05 19:13:09 -0800

    fix text me sizing

--- src/components/profile/Cta.js
+++ src/components/profile/Cta.js
@@ -6,10 +6,10 @@ import CtaDownloadWidget from './CtaDownloadWidget';
 
 const Cta = () => {
   return (
-    <div className="row cta-margin">
-      <div className="col-lg-5  col-md-5 cta cta-text">
+    <div className="row cta-margin cta">
+      <div className="col-lg-5  col-md-5 cta-text">
       This profile was created about you based on your Twitter feed and quiz answers about you by your network. Is this correct? Download Good&Co app to find out.</div>
-      <div className="col-lg-7  col-md-7 cta"><CtaDownloadWidget/></div>
+      <div className="col-lg-7  col-md-7"><CtaDownloadWidget/></div>
     </div>
   )
 
